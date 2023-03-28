3f0159de43cabdd0fdaa6459e32bc8a22e9a13d8
CSCE-111 <126281427+CSCE-111@users.noreply.github.com>
2023-03-28 15:27:51 -0500

    3/27/2023 In class code
    includes controls.java and our hello world in classes.

---
+++ myHW.java
@@ -0,0 +1,18 @@
+/*
+Robert Lightfoot
+class example
+ */
+class myHW{
+  myHW() {
+    System.out.println("A new default constructor.");
+  }
+  myHW(int i) {
+    System.out.println("A second default constructor.");
+  }
+  public static void out(){
+    System.out.println("Hello World.");
+  }
+  public static void outs(){
+    System.out.println("Hola mundo.");
+  }
+}
