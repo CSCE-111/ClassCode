5086b522f86d2d9f0b7fd7df2a4c278360e44703
rslight <rob.light@tamu.edu>
2023-03-28 15:34:58 -0500

    First Test

---
+++ test.java
@@ -0,0 +1,10 @@
+/*
+ * our comments
+ * Robert Lightfoot
+ * Test program
+ */
+class test{
+    public static void main(String[] args) {
+        System.out.println("Here is our test of inclass code.");
+    }
+}
