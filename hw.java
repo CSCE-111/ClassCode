edb09e3c289153769da872a1b8ec26e91b045c42
rslight <rob.light@tamu.edu>
2023-03-28 10:21:19 -0500

    First commit to Git

---
+++ hw.java
@@ -0,0 +1,11 @@
+/*
+Robert Lightfoot
+CSCE 111
+3/28/2023
+Hello World, Git example
+*/
+class hw{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
